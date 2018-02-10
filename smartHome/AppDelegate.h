318193c3dec7c0cdb3903bfa382779cb11bef698
tanxianjing <xj1079139185@126.com>
2018-02-10 10:05:33 +0800

    Initial Commit

---
+++ smartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  smartHome
+//
+//  Created by bang on 2018/2/10.
+//  Copyright © 2018年 XiaoHuiBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
